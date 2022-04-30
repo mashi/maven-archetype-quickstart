3afb77f88abfb74d76c822792056856fd4d2d214
mashi <59000162+mashi@users.noreply.github.com>
2022-04-30 20:07:40 -0300

    add package-file.java

---
+++ src/main/java/com/example/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This is the package-file.java file. This information is placed before the javadoc information from each class.
+ * 
+ * It is possible to use package annotations like this: see the {@link com.example.App} class.
+ * 
+ * @since 1.0
+ * @see com.example
+ */
+package com.example;
