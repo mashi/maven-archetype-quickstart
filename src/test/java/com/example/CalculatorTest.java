7a499afe4adb8998299b5c23b2f914fdc52527a8
mashi <59000162+mashi@users.noreply.github.com>
2022-05-07 20:32:26 -0300

    divide method and exception test

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,5 +12,13 @@ public class CalculatorTest {
         int expected = 2;
         int actual = calculator.add(1, 1);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDivideZeroException(){
+        Calculator calculator = new Calculator();
+        Exception exception = assertThrows(ArithmeticException.class, () ->
+            calculator.divide(1, 0));
+        assertEquals("/ by zero", exception.getMessage());
     }
 }
