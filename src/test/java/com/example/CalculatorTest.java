c5309f0f2bf6a5b40b7f4cb56536d120699a6f4d
mashi <59000162+mashi@users.noreply.github.com>
2022-05-07 22:28:29 -0300

    fix: code format (#10)

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -6,19 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    @Test
-    void testAddTwo(){
-        Calculator calculator = new Calculator();
-        int expected = 2;
-        int actual = calculator.add(1, 1);
-        assertEquals(expected, actual);
-    }
+  @Test
+  void testAddTwo() {
+    Calculator calculator = new Calculator();
+    int expected = 2;
+    int actual = calculator.add(1, 1);
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    void testDivideZeroException(){
-        Calculator calculator = new Calculator();
-        Exception exception = assertThrows(ArithmeticException.class, () ->
-            calculator.divide(1, 0));
-        assertEquals("/ by zero", exception.getMessage());
-    }
+  @Test
+  void testDivideZeroException() {
+    Calculator calculator = new Calculator();
+    Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(1, 0));
+    assertEquals("/ by zero", exception.getMessage());
+  }
 }
