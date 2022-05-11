43d4fd40d348cef9610369e9909786b138c50c9f
mashi <59000162+mashi@users.noreply.github.com>
2022-05-11 13:59:23 -0300

    feat: obfuscate jar with proguard (#14)

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +11,12 @@ public class AppTest {
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
+  }
+
+  @Test
+  public void checkMessage() {
+    String expected = "Hello World!";
+    String actual = App.message();
+    assertEquals(expected, actual);
   }
 }
